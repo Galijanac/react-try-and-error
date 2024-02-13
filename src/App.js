aa2791a255584607ef024651a743ed206825a6b7
nikola <nikolaet@gmail.com>
2024-02-14 00:25:55 +0100

    Random-color added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
+import RandomColor from './components/random-color';
 
 function App() {
   return (
     <div className="App">
         {/* Accordian component */}
-        <Accordian/>
+        {/* <Accordian/> */}
+        {/* Random color component */}
+        <RandomColor/>
     </div>
   );
 }
