6481431884c97622fe5f4d832cebccf30f943441
nikola <nikolaet@gmail.com>
2024-03-03 22:56:28 +0100

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TabTest from './components/custom-tabs/tab-test.jsx';
 import ModalTest from './components/custom-modal-popup/modal-test.jsx';
 import GithubProfileFInder from './components/github-profile-finder/index.jsx';
 import SearchAutoComplete from './components/search-autocomplete-with-api/index.jsx';
+import TicTacToe from './components/tic-tac-toe/index.jsx';
 
 function App() {
   return (
@@ -55,7 +56,10 @@ function App() {
         {/* <GithubProfileFInder /> */}
 
         {/* Search Autocomplete component  */}
-        <SearchAutoComplete />
+        {/* <SearchAutoComplete /> */}
+
+        {/* Tic Tac Toe component */}
+        <TicTacToe />
 
     </div>
   );
